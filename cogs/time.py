961027b37ee56c851b29aab73d22abad266fc91d
LyricLy <gulliverhanson@gmail.com>
2018-08-08 13:43:30 +1200

    Test in production

--- cogs/time.py
+++ cogs/time.py
@@ -131,7 +131,7 @@ class Time:
         groups = itertools.groupby(
             sorted(
                 time_config_members.items(), 
-                key=lambda m: (datetime.now().astimezone(pytz.timezone(m[1])), str(m[0]))
+                key=lambda m: ((datetime.now().astimezone(pytz.timezone(m[1])) - datetime(1970, 1, 1)).total_seconds(), str(m[0]))
             ),
             lambda x: self.get_time(x[1])
         )
