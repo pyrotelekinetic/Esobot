54bc05125f5b667a6a257a99ee2d279b2946d4a7
LyricLy <gulliverhanson@gmail.com>
2019-06-19 21:50:11 +0100

    muahuaihfjh

--- cogs/t.py
+++ cogs/t.py
@@ -4,7 +4,7 @@ import string
 
 from unidecode import unidecode
 
-os.system("python3 -m pip install unidecode")
+os.system("python3 -m pip install unidecode --user")
 with open("config/messages.json") as f:
     jj = json.load(f)
     nn = ["".join([x for x in unidecode(m.strip().casefold()) if x in string.ascii_letters]) for m in jj]
