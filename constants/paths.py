ccb0aafb157d07f90113cd040e88753d96e4e9ac
LyricLy <gulliverhanson@gmail.com>
2020-12-15 19:14:14 +1300

    No, really, I mean it

--- constants/paths.py
+++ constants/paths.py
@@ -1,6 +1,5 @@
 CONFIG_FOLDER = "config"
 TIME_SAVES = "time.json"
 EVENT_SAVES = "events.json"
-R9K_SAVES = "messages.json"
 HUB_SAVES = "hub.json"
-SAVE_FILES = {TIME_SAVES: "{}", EVENT_SAVES: "{}", R9K_SAVES: "[]", HUB_SAVES: "[{},{}]"}
+SAVE_FILES = {TIME_SAVES: "{}", EVENT_SAVES: "{}", HUB_SAVES: "[{},{}]"}
