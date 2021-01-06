d2a34d96c99d34ee0716bc5e9674bfad3a671ba1
LyricLy <gulliverhanson@gmail.com>
2021-01-06 15:43:49 +1300

    Change ZWSP to WJ

--- cogs/game.py
+++ cogs/game.py
@@ -101,7 +101,7 @@ class Games(commands.Cog):
         await asyncio.sleep(10)
 
         prompt = " ".join(random.choices(self.words, k=words))
-        zwsp = "\u200b"
+        zwsp = "\u2060"
 
         self.bot.loop.create_task(self.screen_abr(ctx))
         start = await ctx.send(zwsp.join(list(prompt.translate(str.maketrans({
