7e92225514ef16ff9e2392dc9d0a191791f307ce
Christina Hanson <christinahansondesu@gmail.com>
2022-12-18 12:40:51 +1300

    Remove coral and pyro, and add sofia, to soup time

--- cogs/temp.py
+++ cogs/temp.py
@@ -177,7 +177,7 @@ class Temporary(commands.Cog):
     @is_in_esolangs
     @is_olivia
     async def _time3(self, ctx):
-        await ctx.send("<@636797375184240640> <@309787486278909952> <@319753218592866315> <@224379582785126401> <@402456897812168705> <@261243340752814085> <@166910808305958914> soup time !!")
+        await ctx.send("<@636797375184240640> <@309787486278909952> <@319753218592866315> <@224379582785126401> <@166910808305958914> <@275982450432147456> soup time !!")
 
     @commands.Cog.listener()
     async def on_message(self, message):
