68c7b753b0e7a42e275bac632629d845d65dbad8
Carter Ison <carter@isons.org>
2023-07-04 09:08:11 -0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+
+inputs = {
+  nixpkgs = {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    ref = "nixos-unstable";
+  };
+};
+
+outputs = { self, nixpkgs }: let
+  pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  jishaku = pkgs.callPackage ./jishaku.nix {};
+  discord-ext-menus = pkgs.callPackage ./discord-ext-menus.nix {};
+in {
+  packages.x86_64-linux.default = pkgs.python311Packages.buildPythonPackage {
+      pname = "esobot";
+      version = "0.1.0";
+      format = "setuptools";
+      disabled = pkgs.python311Packages.pythonOlder "3.11";
+
+      src = self;
+
+      propagatedBuildInputs = with pkgs.python311Packages; [
+        discordpy
+        discord-ext-menus
+        unidecode
+        pykakasi
+        openai
+        jishaku
+        pillow
+        dateparser
+        pyahocorasick
+      ];
+
+      preBuild = ''
+cat > setup.py << EOF
+from setuptools import setup
+setup(
+  name='esobot',
+  version='0.1.0',
+  packages=['cogs', 'constants'],
+  py_modules=['utils'],
+  scripts=['main.py']
+)
+EOF
+'';
+
+      doCheck = false;
+    };
+};
+
+}
