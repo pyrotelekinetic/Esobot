d1ea5bb48b34dce3f7b202272cb89ce2446e3d5b
olus2000 <alsabak@gmail.com>
2023-07-12 21:17:17 +0200

    Forced images to match

--- cogs/qwd.py
+++ cogs/qwd.py
@@ -44,17 +44,17 @@ def render_height_graph(height_member):
     # Dimensions: len*60 + 60 x 360
     # Margins: 30 x 20
     height_member.sort(key=lambda x: -x[0])
-    base = Image.new('RGBA', (len(height_member * 60) + 60, 360), (200, 200, 200))
+    base = Image.new('RGBA', (len(height_member * 60) + 60, 360), (200, 200, 200, 255))
     max_height, min_height = height_member[0][0], height_member[-1][0]
     height_dif = max_height - min_height
 
     for i, (height, member, avatar) in enumerate(height_member):
         bar_height = math.ceil((height - min_height) * 280 / height_dif) + 20
-        avatar = Image.open(BytesIO(avatar)).resize((60, bar_height))
+        avatar = Image.open(BytesIO(avatar)).resize((60, bar_height)).convert('RGBA')
         base.alpha_composite(avatar, (60 * i + 30, 340 - bar_height))
 
     draw = ImageDraw.Draw(base)
-    draw.line([(30, 20), (30, 340), (len(height_member) * 60 + 30, 340)], (0, 0, 0), 1)
+    draw.line([(30, 20), (30, 340), (len(height_member) * 60 + 30, 340)], (0, 0, 0, 255), 1)
     # TODO: labels, title, etc.
     rendered = BytesIO()
     base.save(rendered, format='png')
